93f8d6eb92346f79b57d953f0a800ee1e282ca97
Matheus Rodrigues <matheussr61@gmail.com>
2021-12-09 17:08:16 -0300

    stack implementation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module compilador-2021-2
+
+go 1.17
---
+++ src/stack/stack.go
@@ -0,0 +1,71 @@
+package stack
+
+import "fmt"
+
+var (
+	ErrNotEnoughStackSpace = fmt.Errorf("no space left on the stack")
+	ErrEmptyStack          = fmt.Errorf("empty stack")
+)
+
+type stack struct {
+	data     []interface{}
+	capacity int
+	length   int
+}
+
+//NewStack returns a new empty stack with
+//maximum capacity defined by capacity
+func NewStack(capacity int) *stack {
+	return &stack{
+		data:     nil,
+		capacity: capacity,
+		length:   0,
+	}
+}
+
+//Push pushes the element present on the
+//parameter element onto the stack. This
+//is only possible if there is enough space
+//on the stack, otherwise it will return an
+//error and no element will be pushed on the
+//stack
+func (s *stack) Push(element interface{}) error {
+	if s.length >= s.capacity {
+		return ErrNotEnoughStackSpace
+	}
+
+	s.data = append(s.data, element)
+	s.length += 1
+	return nil
+}
+
+//Pop returns and remove the top element of the stack
+//This is only possible if the stack is not empty,
+//otherwise it will return an error
+func (s *stack) Pop() (interface{}, error) {
+	if s.length == 0 {
+		return nil, ErrEmptyStack
+	}
+	element := s.data[s.length-1]
+	s.data = s.data[:s.length-1]
+	s.length -= 1
+	return element, nil
+}
+
+//Returns the length of stack this represents
+//how many elements are present on the stack
+func (s *stack) GetLength() int {
+	return s.length
+}
+
+//Returns the capacity of the stack, this
+//represents the maximum amount of elements
+//that the instance of the stack can hold
+func (s *stack) GetCapacity() int {
+	return s.capacity
+}
+
+//Returns a string representation of the stack
+func (s *stack) String() string {
+	return fmt.Sprintf("{\nData:%v,\nCapacity:%v,\nLength:%v,\n}", s.data, s.capacity, s.length)
+}
