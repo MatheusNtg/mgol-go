72863bde2f0c57b3b4db5c4ae783ca7a6b634dcf
Matheus Rodrigues <matheussr61@gmail.com>
2021-12-20 20:58:47 -0300

    token implementation

---
+++ src/lexer/token.go
@@ -0,0 +1,42 @@
+package lexer
+
+type TokenClass int
+
+// Available classes of tokens
+const (
+	NUM TokenClass = iota
+	LITERAL_CONST
+	IDENTIFIER
+	COMMENT
+	EOF
+	REL_OP
+	ATTR
+	ARIT_OP
+	OPEN_PAR
+	CLOSE_PAR
+	SEMICOLON
+	ERROR
+)
+
+type DataType int
+
+// Available types of data
+const (
+	INTEGER DataType = iota
+	REAL
+	LITERAL
+)
+
+type Token struct {
+	class    TokenClass
+	lexeme   string
+	dataType DataType
+}
+
+func NewToken(class TokenClass, lexeme string, dataType DataType) *Token {
+	return &Token{
+		class:    class,
+		lexeme:   lexeme,
+		dataType: dataType,
+	}
+}
