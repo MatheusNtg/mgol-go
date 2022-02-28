6fff9748af5498781c8cd408ca4c9ae82742d6ad
Matheus Rodrigues <matheussr61@gmail.com>
2022-02-28 13:28:26 -0300

    add grammar

--- src/main.go
+++ src/main.go
@@ -2,35 +2,36 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"mgol-go/src/lexer"
-	"os"
+	"mgol-go/src/parser"
 )
 
 var separator = "=================="
 
 func main() {
-	filePath := os.Args[1]
+	// filePath := os.Args[1]
 
-	file, err := os.Open(filePath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
+	// file, err := os.Open(filePath)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer file.Close()
 
-	symbolTable := lexer.GetSymbolTableInstance()
+	// symbolTable := lexer.GetSymbolTableInstance()
 
-	lexer.FillSymbolTable(symbolTable)
-	defer symbolTable.Cleanup()
+	// lexer.FillSymbolTable(symbolTable)
+	// defer symbolTable.Cleanup()
 
-	scanner := lexer.NewScanner(file, symbolTable)
-	for {
-		token := scanner.Scan()
-		fmt.Println(token)
-		if token == lexer.EOF_TOKEN {
-			break
-		}
-	}
-	fmt.Printf(separator + "\nTabela de símbolos\n" + separator + "\n")
-	symbolTable.Print()
+	// scanner := lexer.NewScanner(file, symbolTable)
+	// for {
+	// 	token := scanner.Scan()
+	// 	fmt.Println(token)
+	// 	if token == lexer.EOF_TOKEN {
+	// 		break
+	// 	}
+	// }
+	// fmt.Printf(separator + "\nTabela de símbolos\n" + separator + "\n")
+	// symbolTable.Print()
+
+	fmt.Println(parser.GetRulesMap("./parser/grammar.json"))
+
 }
